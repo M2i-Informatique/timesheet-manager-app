d434f61e929b0cbe5c1cf041ed308fcd13a3aa86
Lucas <lucas@informatique-m2i.fr>
2025-12-01 15:55:54 +0100

    feat: ajout de la Zone 7 Majorée avec calcul au kilomètre
    - Ajout de la colonne 'is_per_km' à la table zones
    - Création du seeder pour la Zone 7 Majorée (> 60km, 0.17€/km)
    - Implémentation du calcul variable dans CostsCalculator
    - Mise à jour du modèle Zone

--- app/Models/Zone.php
+++ app/Models/Zone.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Zone extends Model
 {
-    protected $fillable = ['name', 'min_km', 'max_km', 'rate'];
+    protected $fillable = ['name', 'min_km', 'max_km', 'rate', 'is_per_km'];
 
-    protected $casts = ['min_km' => 'float', 'max_km' => 'float', 'rate' => 'float'];
+    protected $casts = ['min_km' => 'float', 'max_km' => 'float', 'rate' => 'float', 'is_per_km' => 'boolean'];
 
     /**
      * Get all the projects for the zone.
